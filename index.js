3a35ce4811be6edd7a6f53f41a8308be96b40321
rishabh570 <www.rishabhrawat570@gmail.com>
2020-02-07 15:45:19 +0530

    Add error handler.

--- index.js
+++ index.js
@@ -5,21 +5,27 @@ const prompts = require('./lib/prompts'),
 	  helper = require('./lib/helper'),
 	  matchObs = require('./lib/matchObs'),
 	  CLUI = require('clui'),
-	  Spinner = CLUI.Spinner;
+	  Spinner = CLUI.Spinner,
+	  {handler} = require('./lib/error_handler');
 
 
 (async () => {
 	output.printName(); // Prints "cricfeed"
 
 	// Prompts user for type of matches
-	const type = await prompts.askTypeOfMatch();
+	const [type, typeErr] = await handler(prompts.askTypeOfMatch());
+	if(typeErr) output.throwError('Type of match not received, please select one!');
+
+	// TODO: Check user's internet connection and throw error if offline
 
 	// Loading spinner starts
 	const spinner_first = helper.makeSpinner(type);
 	spinner_first.start();
 
 	// Fetches relevant live matches
-	let liveMatches = await fetcher.getLiveMatches();
+	const [liveMatches, liveMatchesErr] = await handler(fetcher.getLiveMatches());
+	if(liveMatchesErr) output.throwError('Could not fetch live matches, please try again!');
+
 	// Filter out matches according to the user's query
 	let matches = helper.getMatches(liveMatches, type);
 
@@ -29,9 +35,10 @@ const prompts = require('./lib/prompts'),
 	spinner_first.stop(); // Stop the spinner
 
 	// Prompt the user to select a match from the list of choices
-	const match = await prompts.askForMatch(choices);
+	const [match, matchErr] = await handler(prompts.askForMatch(choices));
+	if(matchErr) output.throwError('Could not fetch data for the match, please try again!')
 
-	// Todo: ADD SPINNER HERE (THINK ABOUT EXPORTING THE SPINNER OBJECT)
+	// TODO: ADD SPINNER HERE (THINK ABOUT EXPORTING THE SPINNER OBJECT)
 
 	const ID = matches[choices.indexOf(match)].match_id; // ID of the match user selected
 	matchObs(ID); // Create an observable that keeps track of the stats for the desired match and shows them to the user

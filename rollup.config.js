8c151934d385a67850a93a07e2080654f9d9872c
rishabh570 <www.rishabhrawat570@gmail.com>
2020-03-12 20:35:29 +0530

    add node polyfills and remove local installation of internal packages.

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 // import { terser } from "rollup-plugin-terser";
 import json from '@rollup/plugin-json';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default {
     input: './index.js',
@@ -10,9 +11,8 @@ export default {
         format: 'cjs',
     },
     plugins: [
-        resolve({
-            preferBuiltins: true,
-        }),
+        nodePolyfills(),
+        resolve(),
         commonjs(),
         json(),
         // terser()
